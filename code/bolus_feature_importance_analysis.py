3f56647423a656d8c73e6da184c6826a539eb345
AQ <anna.quinlan123@gmail.com>
2020-06-05 20:44:29 -0700

    Add analysis of data features

---
+++ code/bolus_feature_importance_analysis.py
@@ -0,0 +1,39 @@
+import shap
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+
+from sklearn.ensemble import IsolationForest
+
+from bolus_risk_analysis import extract_and_process_boluses, train_model
+
+
+# Path to the file of processed doses - YOU MUST FILL THIS IN
+path_to_file = "TO_FILL_IN.csv"
+df = extract_and_process_boluses(pd.read_csv(path_to_file))
+
+# Get trained model model
+data_to_predict = df[
+        [
+            "totalBolusAmount", 
+            "carbInput", 
+            #"insulinCarbRatio", 
+            "bgInput", 
+            "insulinSensitivity", 
+            "TDD",
+            "bg_30_min_before", 
+            "bg_75_min_after"
+        ]
+    ]
+trained_model = train_model(data_to_predict, "isolation_forest")
+# Explain the model's predictions using SHAP
+explainer = shap.TreeExplainer(trained_model)
+shap_values = explainer.shap_values(data_to_predict)
+
+# Plot the variables, ranked by impact on abnormality score
+shap.summary_plot(shap_values, data_to_predict, plot_type="bar")
+
+# For each data feature, plot the impact on abnormality score vs value (negative -> more abnormal)
+for col in data_to_predict.columns:
+    shap.dependence_plot(col, shap_values, data_to_predict)
+
--- code/bolus_risk_analysis.py
+++ code/bolus_risk_analysis.py
@@ -12,6 +12,79 @@ from datetime import datetime, timedelta
 
 
 def find_abnormal_boluses(processed_df, model_type="knn"):
+    df = extract_and_process_boluses(processed_df)
+
+    # Print some summary statistics
+    print("Head")
+    print(df.head(), "\n")
+    shape = df.shape
+    print("Shape", shape)
+    print(df.describe().apply(lambda s: s.apply(lambda x: format(x, "f"))))
+
+    data_to_predict = df[
+        [
+            "totalBolusAmount", 
+            "carbInput", 
+            "insulinCarbRatio", 
+            "bgInput", 
+            "insulinSensitivity", 
+            "TDD",
+            "bg_30_min_before", 
+            "bg_75_min_after"
+        ]
+    ]
+    model = train_model(data_to_predict, model_type)
+    predictions = model.predict(data_to_predict)
+    df["abnormal"] = predictions
+    if model_type == "isolation_forest":
+        df["abnormality_score"] = model.decision_function(data_to_predict)
+    unique, counts = np.unique(predictions, return_counts=True)
+    
+    # Plot the results
+    # Note that this plot only incorporates 3 dimensions of the data, so there are other 
+    # factors present that might explain why certain values were/were not classified as outliers
+    '''
+    fig = plt.figure(1, figsize=(7,7))
+    ax = Axes3D(fig, rect=[0, 0, 0.95, 1], elev=48, azim=134)
+    ax.scatter(df["totalBolusAmount"], df["carbInput"], df["TDD"],
+            c=predictions, edgecolor="k", s=50)
+    ax.set_xlabel("Bolus Amount")
+    ax.set_ylabel("Carb Amount")
+    ax.set_zlabel("TDD")
+
+    plt.title("IF To Classify Boluses" if model_type == "isolation_forest" else "KNN To Classify Boluses", fontsize=14)
+    plt.show()
+    '''
+
+    # Select our abnormal rows
+    if model_type == "isolation_forest":
+        # Isolation forest: 1 is normal, -1 is abnormal
+         print ("Outliers:", counts[0], "\nTotal:", shape[0], "\nPercent:", round(counts[0]/shape[0] * 100), "%")
+         abnormals = df.query('abnormal == -1')
+    else:
+        # KNN: 0 is normal, 1 is abnormal
+        print ("Outliers:", counts[1], "\nTotal:", shape[0], "\nPercent:", round(counts[1]/shape[0] * 100), "%")
+        abnormals = df.query('abnormal == 1')
+
+    return abnormals
+
+
+def train_model(data_to_predict, model_type):
+    # will want to play around with n_neighbors
+    if model_type == "isolation_forest":
+        # Set a random state for reproducable results
+        rng = np.random.RandomState(42)
+        model = IsolationForest(random_state=rng, contamination=0.05)
+    else:
+        model = KNN()
+
+    model.fit(data_to_predict)
+
+    return model
+
+
+''' Take a dataframe of processed dose values and extract the doses from it '''
+def extract_and_process_boluses(processed_df):
     # Create a df with the data relevent to boluses
     df = processed_df[
         [
@@ -58,65 +131,4 @@ def find_abnormal_boluses(processed_df, model_type="knn"):
     # Convert the time strings to pandas datetime format
     df["time"] = pd.to_datetime(df["time"], infer_datetime_format=True)
 
-    # Print some summary statistics
-    print("Head")
-    print(df.head(), "\n")
-    shape = df.shape
-    print("Shape", shape)
-    print(df.describe().apply(lambda s: s.apply(lambda x: format(x, "f"))))
-
-    # will want to play around with n_neighbors
-    if model_type == "isolation_forest":
-        # Set a random state for reproducable results
-        rng = np.random.RandomState(42)
-        model = IsolationForest(random_state=rng, contamination=0.05)
-    else:
-        model = KNN()
-
-    data_to_predict = df[
-        [
-            "totalBolusAmount", 
-            "carbInput", 
-            "insulinCarbRatio", 
-            "bgInput", 
-            "insulinSensitivity", 
-            "TDD",
-            "bg_30_min_before", 
-            "bg_75_min_after"
-        ]
-    ]
-    model.fit(data_to_predict)
-
-    predictions = model.predict(data_to_predict)
-    df["abnormal"] = predictions
-    if model_type == "isolation_forest":
-        df["abnormality_score"] = model.decision_function(data_to_predict)
-    unique, counts = np.unique(predictions, return_counts=True)
-    
-    # Plot the results
-    # Note that this plot only incorporates 3 dimensions of the data, so there are other 
-    # factors present that might explain why certain values were/were not classified as outliers
-    '''
-    fig = plt.figure(1, figsize=(7,7))
-    ax = Axes3D(fig, rect=[0, 0, 0.95, 1], elev=48, azim=134)
-    ax.scatter(df["totalBolusAmount"], df["carbInput"], df["TDD"],
-            c=predictions, edgecolor="k", s=50)
-    ax.set_xlabel("Bolus Amount")
-    ax.set_ylabel("Carb Amount")
-    ax.set_zlabel("TDD")
-
-    plt.title("IF To Classify Boluses" if model_type == "isolation_forest" else "KNN To Classify Boluses", fontsize=14)
-    plt.show()
-    '''
-
-    # Select our abnormal rows
-    if model_type == "isolation_forest":
-        # Isolation forest: 1 is normal, -1 is abnormal
-         print ("Outliers:", counts[0], "\nTotal:", shape[0], "\nPercent:", round(counts[0]/shape[0] * 100), "%")
-         abnormals = df.query('abnormal == -1')
-    else:
-        # KNN: 0 is normal, 1 is abnormal
-        print ("Outliers:", counts[1], "\nTotal:", shape[0], "\nPercent:", round(counts[1]/shape[0] * 100), "%")
-        abnormals = df.query('abnormal == 1')
-
-    return abnormals
+    return df
